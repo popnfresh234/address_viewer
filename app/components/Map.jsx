ea0eb28829cf9e37b270d80ae493da9dc610c5bc
Alex <ajhollid@gmail.com>
2020-05-19 22:59:52 -0700

    implement info window

--- app/components/Map.jsx
+++ app/components/Map.jsx
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-
+import { InfoWindow } from '@react-google-maps/api';
 
 class Map extends Component {
   constructor( props ) {
     super( props );
+    this.onMarkerClick = this.onMarkerClick.bind( this );
+    this.onCloseClick = this.onCloseClick.bind( this );
     this.CAP_HEIGHTS = { lat: 49.3584482, lng: -123.1068815 };
     this.CONTAINER_STYLE = {
       width: '100vw',
       height: '100vh',
     };
     this.state = {
-      customers: [],
       markers: [],
+      showInfo: false,
+      infoPos: {},
     };
   }
+
 
   componentDidMount() {
     fetch( '/data/' )
@@ -22,6 +26,19 @@ class Map extends Component {
       .then( ( markers ) => {
         this.setState( { markers } );
       } );
+  }
+
+  onMarkerClick( marker ) {
+    this.setState( {
+      showInfo: true,
+      infoPos: marker,
+    } );
+  }
+
+  onCloseClick() {
+    this.setState( {
+      showInfo: false,
+    } );
   }
 
 
@@ -34,7 +51,14 @@ class Map extends Component {
           zoom={12}
           onLoad={this.onMapLoad}
         >
-          {this.state.markers.map( marker => <Marker position={marker} key={marker.lng} /> )}
+          {this.state.markers.map( marker => <Marker position={marker} key={Math.random()} onClick={e => this.onMarkerClick( marker, e )} /> )}
+          {this.state.showInfo &&
+          <InfoWindow
+            position={this.state.infoPos}
+            onCloseClick={this.onCloseClick}
+          >
+            <h1>{this.state.infoPos.lat}</h1>
+          </InfoWindow>}
         </GoogleMap>
       </LoadScript>
     );
